92dee7f2980a84a5cc68fe52fd3d07c33dcbfbc4
LindeBee <38701378+LindeBee@users.noreply.github.com>
2020-12-01 13:52:47 +0100

    folder for lab 2

---
+++ lab2/README.md
@@ -0,0 +1,1 @@
+This folder contains all files related to the second lab of the Scalable Machine Learning course.
---
+++ lab2/lab2.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+
+def f(x):
+    return np.sin(x)
+
+xTrain = np.arange(0, 2*np.pi, 0.01)
+yTrain = f(xTrain)
+xTest = np.arange(0.05, 2*np.pi, 0.1)
+yTest = f(xTest)
+
+fig = plt.subplot()
+fig.scatter(xTrain,yTrain)
+plt.show()
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(100, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(49, activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(1))
+model.compile(loss='mean_squared_error')
+
+model.fit(xTrain, yTrain, batch_size = 100, epochs = 300, shuffle = True)
+pTest = model.predict(xTest)
+pTrain = model.predict(xTrain)
+
+fig = plt.subplot()
+fig.scatter(xTest,pTest)
+fig.scatter(xTest,yTest)
+fig.scatter(xTrain,pTrain)
+plt.show()
+
